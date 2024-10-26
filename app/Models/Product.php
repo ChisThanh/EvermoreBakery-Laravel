a5016ebb2e05ab399a6242b29b9ee0fa099f7952
chisthanh <chisthanhdev@gmail.com>
2024-10-26 16:22:44 +0000

    add crud

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Backpack\CRUD\app\Models\Traits\CrudTrait;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -9,6 +10,7 @@ use Laravel\Scout\Searchable;
 
 class Product extends Model
 {
+    use CrudTrait;
     use HasFactory;
     use SoftDeletes;
     use Searchable;
