fd3e915babbea3e1be2884679bd5fd0686bb05c4
chisthanh <chisthanhdev@gmail.com>
2024-11-18 09:27:12 +0000

    update code

--- database/migrations/2024_02_25_090657_create_product_reviews_table.php
+++ database/migrations/2024_02_25_090657_create_product_reviews_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -17,6 +16,7 @@ return new class extends Migration
             $table->foreignId('product_id')->constrained();
             $table->float('rating', 10)->default(0);
             $table->string('comment', 100)->default('');
+            $table->boolean('is_show')->default(true);
             $table->timestamps();
         });
     }
@@ -27,6 +27,6 @@ return new class extends Migration
     public function down(): void
     {
         Schema::dropIfExists('product_reviews');
-        
+
     }
 };
