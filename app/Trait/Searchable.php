6c3582045a14804862fe386b0ac6020dde2a56a8
chisthanh <chisthanhdev@gmail.com>
2024-11-23 14:26:04 +0000

    update code

--- app/Trait/Searchable.php
+++ app/Trait/Searchable.php
@@ -1,41 +1,34 @@
 <?php
-
-declare(strict_types=1);
-
-namespace App\Traits;
-
 /**
  * Created by ChisThanh.
  * User: ChisThanh
  * Date: 23/11/2024
  */
 
+declare(strict_types=1);
+
+namespace App\Traits;
+
 use Illuminate\Database\Eloquent\Builder;
 
 /**
  * Trait Searchable
  * @package App\Traits
- * @property array $searchableColumns
- * @property array $joins
+ * @property array $searchable
  * @property array $dateColumns
- * @method static \Illuminate\Database\Eloquent\Builder search(string $term, array $option = [])
+ * @method static \Illuminate\Database\Eloquent\Builder search(string $search, array $option = [])
  */
 
 trait Searchable
 {
-    protected $searchableColumns = [
-        'name',
-        'description',
-        'categories.name'
-    ];
-
-    protected $joins = [
-        [
-            'table' => 'categories',
-            'first' => 'products.category_id',
-            'operator' => '=',
-            'second' => 'categories.id',
-            'type' => 'LEFT'
+    protected $searchable = [
+        'columns' => [
+            'users.first_name',
+            'users.last_name',
+            'posts.title',
+        ],
+        'joins' => [
+            'posts' => ['users.id', 'posts.user_id'],
         ]
     ];
 
@@ -49,42 +42,36 @@ trait Searchable
      */
     public function bootSearchable(): void
     {
-        if (empty($this->searchableColumns)) 
-            throw new \Exception('Class must declare $searchableColumns property.');
-        
-        if(env('DB_CONNECTION') !== 'mysql') 
+        if (empty($this->searchable['columns']))
+            throw new \Exception('Class must declare $searchable property.');
+
+        if (env('DB_CONNECTION') !== 'mysql')
             throw new \Exception('Searchable trait only support for MySQL database.');
     }
 
-
     /**
      * @param $query
-     * @param string $term
+     * @param string $search
      * @param array $option
      * @return mixed
      */
-    public function scopeSearch(Builder $query, string $term, array $option = []): mixed
+    public function scopeSearch(Builder $q, string $search, array $option = []): mixed
     {
         // ALTER TABLE products
         // ADD FULLTEXT(name, description);
+        $query = clone $q;
 
-        if (empty($term) || $term === ' ')
+        if (empty($search) || $search === '')
             return $query;
 
-        if (!empty($this->joins) && is_array($this->joins)) {
-            foreach ($this->joins as $join) {
-                $joinType = strtoupper($join['type'] ?? 'INNER');
-                $query->join($join['table'], $join['first'], $join['operator'], $join['second'], $joinType);
-            }
-        }
-
-        $term = $this->cleanSearchTerm($term);
-        $query = $this->generateTerm($query, $term);
+        $query = $this->makeJoin($query);
+        $search = $this->cleanSearchTxt($search);
+        $query = $this->generateSearch($query, $search);
 
         if (!empty($option) && is_array($option)) {
             $query->where(function ($q) use ($option) {
                 foreach ($option as $value) {
-                    $operator = $this->parseOperator($value['operator'] ?? '=');
+                    $operator = $this->parseOperator($value['operator'] ?? 'eq');
 
                     if ($this->isDateColumn($value['column']))
                         $q->whereDate($value['column'], $operator, $value['q']);
@@ -102,62 +89,45 @@ trait Searchable
                 }
             });
         }
+        return $query;
+    }
 
-        if (!empty($option['sort']) && is_array($option['sort'])) {
-            foreach ($option['sort'] as $field => $direction) {
-                $query->orderBy($field, $direction);
+    protected function makeJoin(Builder $query): Builder
+    {
+        $joins = $this->searchable['joins'] ?? [];
+        if (!empty($joins) && is_array($joins))
+            foreach ($joins as $table => $join)
+                $query->leftJoin($table, $join[0], '=', $join[1]);
+        return $query;
+    }
+
+    protected function generateSearch(Builder $query, string $search): Builder
+    {
+        $groupedColumns = [];
+
+        foreach ($this->searchable['columns'] as $column) {
+            if (strpos($column, '.') === false) {
+                $groupedColumns['default'][] = $column;
+            } else {
+                [$table, $_] = explode('.', $column, 2);
+                $groupedColumns[$table][] = $column;
             }
         }
 
-        return $query;
-    }
+        foreach ($groupedColumns as $table => $columns) {
+            $columnList = implode(',', $columns);
 
-    protected function generateTerm(Builder $query, string $term): Builder
-    {
-        $str = '';
-        $other = [];
-
-        foreach ($this->searchableColumns as $column) {
-            if (strpos($column, '.') === false)
-                $str .= $column . ',';
+            if ($table === 'default')
+                $query->whereRaw("MATCH ($columnList) AGAINST (? IN BOOLEAN MODE)", [$search]);
+            elseif ($table === $this->getTable())
+                $query->whereRaw("MATCH ($columnList) AGAINST (? IN BOOLEAN MODE)", [$search]);
             else
-                $other[] = $column;
-        }
-
-        $str = rtrim($str, ',');
-        $query->whereRaw("MATCH ($str) AGAINST (? IN BOOLEAN MODE)", [$term]);
-
-        if (!empty($other)) {
-            $column = implode(',', $other);
-            $query->orWhereRaw("MATCH ({$column}) AGAINST (? IN BOOLEAN MODE)", [$term]);
+                $query->orWhereRaw("MATCH ({$columnList}) AGAINST (? IN BOOLEAN MODE)", [$search]);
         }
 
         return $query;
     }
 
-    /**
-     * Map operator aliases to SQL operators.
-     *
-     * @param string $operator
-     * @return string
-     */
-    protected function parseOperator(string $operator): string
-    {
-        return match ($operator) {
-            'eq' => '=',
-            'ne' => '!=',
-            'gt' => '>',
-            'lt' => '<',
-            'gte' => '>=',
-            'lte' => '<=',
-            'like' => 'like',
-            'in' => 'in',
-            'nin' => 'not in',
-            'bw' => 'between',
-            'or' => 'or',
-            default => '=',
-        };
-    }
 
     /**
      * Check if the column is a date column.
@@ -183,17 +153,37 @@ trait Searchable
     /**
      * Clean the search term to prevent SQL injection and other malicious input.
      *
-     * @param string $term
+     * @param string $search
      * @return string
      */
-    protected function cleanSearchTerm(string $term): string
+    protected function cleanSearchTxt(string $search): string
     {
-        $term = htmlspecialchars($term, ENT_QUOTES, 'UTF-8');
-        $term = preg_replace(
-            '/[^\w\s.-áàảãạăắằẳẵặâấầẩẫậêếềểễệíìỉĩịôốồổỗộơớờởỡợưứừửữựđáàảãạăắằẳẵặâấầẩẫậêếềểễệíìỉĩịôốồổỗộơớờởỡợưứừửữựđ]/u',
-            '',
-            $term
-        );
-        return trim($term);
+        $search = htmlspecialchars($search, ENT_QUOTES, 'UTF-8');
+        $search = preg_replace('/[^\p{L}\p{N}\s.-]/u', '', $search);
+        return trim($search);
+    }
+
+    /**
+     * Map operator aliases to SQL operators.
+     *
+     * @param string $operator
+     * @return string
+     */
+    protected function parseOperator(string $operator): string
+    {
+        return match ($operator) {
+            'eq' => '=',
+            'ne' => '!=',
+            'gt' => '>',
+            'lt' => '<',
+            'gte' => '>=',
+            'lte' => '<=',
+            'like' => 'like',
+            'in' => 'in',
+            'nin' => 'not in',
+            'bw' => 'between',
+            'or' => 'or',
+            default => '=',
+        };
     }
 }
